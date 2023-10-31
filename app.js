d342fb471073edaee7ac27e8f649a22c226d91c7
huynq <huynq.meditech@gmail.com>
2023-10-31 09:33:44 +0700

    update

--- app.js
+++ app.js
@@ -1,63 +1,76 @@
 class SpeechRecognitionApi {
-    constructor(options) {
-        const SpeechToText = window.speechRecognition || window.webkitSpeechRecognition;
+  constructor(options) {
+    const SpeechToText =
+      window.speechRecognition || window.webkitSpeechRecognition;
 
-        this.speechApi = new SpeechToText();
-        this.speechApi.continuous = true;
-        this.speechApi.interimResults = false;
+    this.speechApi = new SpeechToText();
+    this.speechApi.continuous = true;
+    this.speechApi.interimResults = false;
 
-        this.speechApi.lang = 'vi-VN';
-        this.output = options.output ? options.output : document.createElement('div');
+    this.speechApi.lang = "vi-VN";
+    this.isRecording = false;
+    this.recordingInterval = null;
+    this.recordCount = 0;
+    this.maxRecordCount = document.querySelectorAll("#table-body tr").length;
+
+    this.startRecording = () => {
+      if (!this.isRecording && this.recordCount < this.maxRecordCount) {
+        this.isRecording = true;
+        console.log("Ghi âm bắt đầu lần thứ", this.recordCount + 1, 'Time 5s');
+        this.speechApi.start();
+        this.speechApi.onresult = (event) => {
+          const result = event.results[0][0].transcript;
+          const tableRows = document.querySelectorAll("#table-body tr");
+
+          if (this.recordCount < tableRows.length - 1) {
+            const outputCell =
+              tableRows[this.recordCount].querySelector(".output");
+                outputCell.textContent = result;
+                console.log('Kết quả record tại thời điểm: ',result)
+          }
+        };
+        this.recordingInterval = setInterval(() => {
+          this.stopRecording();
+
+          if (this.recordCount === this.maxRecordCount - 1) {
+            clearInterval(this.recordingInterval);
+          } else {
+            setTimeout(() => {
+              this.recordCount++;
+              this.startRecording();
+            }, 1000);
+          }
+        }, 5000);
+      }
+    };
+
+    this.stopRecording = () => {
+      if (this.isRecording) {
         this.isRecording = false;
-        this.recordingTimeout = null;
 
-        this.startRecording = () => {
-            this.isRecording = true;
-            console.log('Ghi âm bắt đầu');
-            this.speechApi.start();
-            if (this.recordingTimeout) {
-                clearTimeout(this.recordingTimeout);
-            }
-            this.recordingTimeout = setTimeout(() => {
-                this.stopRecording();
-                console.log('Dừng ghi âm sau 5 giây');
-            }, 5000);
+        this.speechApi.stop();
+        console.log("Dừng ghi âm lần thứ", this.recordCount + 1);
+        clearInterval(this.recordingInterval);
+      }
+    };
 
-            this.speechApi.onresult = (event) => {
-                // Xử lý kết quả ghi âm ở đây
-                
-                const result = event.results[0][0].transcript;
-                this.output.textContent = result;
-                console.log(result)
-            };
-        };
+    this.init();
+  }
 
-        this.stopRecording = () => {
-            this.isRecording = false;
-            console.log('Ghi âm dừng');
-            this.speechApi.stop();
-            if (this.recordingTimeout) {
-                clearTimeout(this.recordingTimeout);
-                this.recordingTimeout = null;
-            }
-        };
+  init() {
+    document.querySelector(".btn-start").addEventListener("click", () => {
+      this.recordCount = 0; // Đặt lại recordCount
+      this.startRecording();
+    });
 
-        this.init();
-    }
-
-    init() {
-        document.querySelector('.btn-start').addEventListener('click', () => {
-            this.startRecording();
-        });
-
-        document.querySelector('.btn-stop').addEventListener('click', () => {
-            this.stopRecording();
-        });
-    }
+    document.querySelector(".btn-stop").addEventListener("click", () => {
+      this.stopRecording();
+    });
+  }
 }
 
 window.onload = function () {
-    var speech = new SpeechRecognitionApi({
-        output: document.querySelector('.output')
-    });
+  var speech = new SpeechRecognitionApi({
+    //output: document.querySelector('.output')
+  });
 };
